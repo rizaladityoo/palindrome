8517601f684df6f1e7c359807d916200c0ae873a
unknown <rizaladityoo@yahoo.com>
2022-10-11 11:38:40 +0700

    tugas

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	kata := "susah"
+
+	fmt.Println(isPalindrom(kata))
+}
+
+func isPalindrom(kata string) bool {
+	var loop int
+	if loop = len(kata) / 2; len(kata)%2 != 0 {
+		loop = (len(kata) - 1) / 2
+	}
+
+	for i := 0; i < loop; i++ {
+		if kata[i] != kata[len(kata)-i-1] {
+			return false
+		}
+	}
+	return true
+}
